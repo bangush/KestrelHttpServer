a21c629618b2caacc20694464884f7d8bbf5dd45
Ben Adams <thundercat@illyriad.co.uk>
2016-11-15 00:42:04 +0000

    Add Request Parsing benchmark

---
+++ perf/Microsoft.AspNetCore.Server.Kestrel.Performance/configs/CoreConfig.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+
+namespace Microsoft.AspNetCore.Server.Kestrel.Performance
+{
+    public class CoreConfig : ManualConfig
+    {
+        public CoreConfig()
+        {
+            Add(Job.Default.
+                With(Platform.X64).
+                With(Jit.RyuJit).
+                With(BenchmarkDotNet.Environments.Runtime.Core).
+                WithRemoveOutliers(true).
+                With(new GcMode() { Server = true }).
+                With(RunStrategy.Throughput).
+                WithLaunchCount(3).
+                WithWarmupCount(5).
+                WithTargetCount(10));
+        }
+    }
+}
